34599d623922683c5e38389dcfaf7dd63ff6c9cd
AidenKerr <arkerr2000@gmail.com>
2021-07-02 22:20:23 -0700

    Add Login and Logout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,75 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import Login from './pages/Login.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
+
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem('user');
+    if (loggedInUser) {
+      //const foundUser = JSON.parse(loggedInUser);
+      setUser(loggedInUser);
+    }
+  }, []);
+
+  const handleLogOut = () => {
+    setUser('');
+    localStorage.clear();
+  }
+
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <Router>
+      <div>
+        <p>{user ? `Hello, ${user}` : '' }</p>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Log In</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to='/' onClick={handleLogOut}>Log Out</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <Login setUser={setUser}/>
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Axios from 'axios';
+import { useHistory } from "react-router";
+
+function Login(props) {
+
+    const [errorMsg, setErrorMsg] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    let history = useHistory();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        let response;
+        try {
+            response = await Axios.post('http://localhost:3001/api/login', {
+                username: username, 
+                password: password
+            });
+        } catch (e) {
+            setErrorMsg("Log In Error");
+            return;
+        }
+        
+        
+
+        // set state of user
+        props.setUser(response.data.result);
+        // store in localStorage
+        localStorage.setItem('user', response.data.result);
+        
+        history.push('/');
+    }
+
+    return (
+        <div className="login-form">
+            <form>
+                Log In
+                <label>Username: <input type="text" name="username" onChange={(e) => {setUsername(e.target.value)}}></input></label>
+                <label>Password: <input type="password" name="password" onChange={(e) => {setPassword(e.target.value)}}></input></label>
+                <button onClick={handleSubmit}>Submit</button>
+                <label className="error">{errorMsg}</label>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
